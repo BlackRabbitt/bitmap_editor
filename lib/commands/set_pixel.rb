e1aa2088147c7aca7756ec3a66f872a0a1848638
Sujit Shakya <sshakya.mail@gmail.com>
2017-10-25 22:33:17 +0545

    Add test cases for set_pixel command and made it pass.

--- lib/commands/set_pixel.rb
+++ lib/commands/set_pixel.rb
@@ -1,23 +1,43 @@
 require './lib/bitmap_array'
 require './lib/bitmap_exception'
 
+require './utils/string'
+require 'pry'
+
 class SetPixel
   attr_reader :err
 
+  include Utils::String
+
   def initialize(args)
-    @args = args
+    @x, @y, @colour, @err = get_parameters(args)
   end
 
-  # # execute command on bitmap_array_matrix with args
-  # def execute_on(bitmap_array)
-  #   return BitmapException.new(BitmapException::ERROR, "Expected to have 3 arguments but instead got #{@args.length}") if @args.length != 3
+  # execute command on bitmap_array_matrix with args
+  def execute_on(bitmap_array)
+    err = BitmapException.new(BitmapException::ERROR, "Couldn't call SetPixel in empty bitmap. Use create() command to create bitmap.") if bitmap_array.length == 0
+    return err unless err.nil?
 
-  #   for i in 1..length
-  #     for j in 1..breadth
-  #       bitmap_array[i][j]="0"
-  #     end
-  #   end
+    bitmap_array[@x][@y] = @colour
 
-  #   return BitmapException.new(BitmapException::WARN, "Create command called twice.") unless bitmap_array.length > 0
-  # end
+    nil
+  end
+
+  private
+  def get_parameters(args)
+    err = BitmapException.new(BitmapException::ERROR, "Invalid arguments `#{args}`") if !args.is_a?(Array)
+    return nil, nil, nil, err unless err.nil?
+
+    err = BitmapException.new(BitmapException::ERROR, "Expected to have 3 arguments but instead got #{args.length}") if args.length != 3
+    return nil, nil, nil, err unless err.nil?
+
+    invalid = ((!is_integer(args[0]) || (args[0].to_i < 1 || args[0].to_i > 250)) || (!is_integer(args[1]) || (args[1].to_i < 1 || args[1].to_i > 250)) ||is_integer(args[2]))
+    err = BitmapException.new(BitmapException::ERROR, "Invalid argument type. Arguments should be (integer, integer, string) where 1<integer<250") if invalid
+    return nil, nil, nil, err unless err.nil?
+
+    x = args[0].to_i
+    y = args[1].to_i
+    c = args[2]
+    return x, y, c, nil
+  end
 end
--- test/commands/set_pixel_test.rb
+++ test/commands/set_pixel_test.rb
@@ -1,11 +1,55 @@
 require 'minitest/autorun'
 
 require './lib/commands/set_pixel'
+require './lib/commands/create'
+
 require './lib/bitmap_array'
 
 class SetPixelTest < MiniTest::Test
   def test_if_exists
     cmd = SetPixel.new(["2", "5", "A"])
     assert_kind_of SetPixel, cmd
+  end
+
+  def test_if_it_handles_invalid_arguments
+    cmd = SetPixel.new("This should not valid")
+    assert nil != cmd.err, "err object should exist for non-array parameter create create command."
+
+    cmd = SetPixel.new(["A", "B", "A"])
+    assert nil != cmd.err, "err object should exist for 1 non-permitted arguments for create command"
+
+    cmd = SetPixel.new(["2", "4", "2"])
+    assert nil != cmd.err, "err object should exist for 2 non-permitted arguments for create command"
+
+    cmd = SetPixel.new(["A", "4", "A"])
+    assert nil != cmd.err, "err object should exist for 3 non-permitted arguments for create command"
+
+    cmd = SetPixel.new(["0", "0", "A"])
+    assert nil != cmd.err, "err object should exist if the arguments is smaller than 1"
+
+    cmd = SetPixel.new(["1", "255", "B"])
+    assert nil != cmd.err, "err object should exist if the arguments is greater than 250"
+
+    cmd = SetPixel.new(["1", "3", "4", "X"])
+    assert nil != cmd.err, "err object should exist if it has extra arguments."
+
+    cmd = SetPixel.new(["250", "1", "D"])
+    assert_nil cmd.err, "This is a valid arguments."
+  end
+
+  def test_if_it_works
+    bitmap_array = BitmapArray.new
+    cmd = SetPixel.new(["4", "2", "A"])
+    err = cmd.execute_on(bitmap_array)
+    assert nil != err, "err object should exist if set_pixel is called in empty bitmap-array. Create should be called first."
+
+    cmd = Create.new(["3", "2"])
+    cmd.execute_on(bitmap_array)
+    cmd = SetPixel.new(["2", "1", "A"])
+    err = cmd.execute_on(bitmap_array)
+
+    assert_nil err, "err object must be nil"
+    assert_equal "A", bitmap_array[2][1], "Pixel (2,1) must be coloured with A"
+    assert_equal "0", bitmap_array[1][2], "Pixel (1,2) must have default 0"
   end
 end
